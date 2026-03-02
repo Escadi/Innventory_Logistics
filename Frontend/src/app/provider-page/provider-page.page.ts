2ce991a478376037c94a6b980873296d529b8205
Escadi <eliuvl86@gmail.com>
2026-03-02 09:59:42 +0000

    cambios en proveedores

--- Frontend/src/app/provider-page/provider-page.page.ts
+++ Frontend/src/app/provider-page/provider-page.page.ts
@@ -8,9 +8,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProviderPagePage implements OnInit {
 
+  providers: any[] = [
+    {
+      id: 1,
+      name: 'Proveedor 1',
+      email: 'proveedor1@gmail.com',
+      phone: '123456789',
+      address: 'Calle 123',
+      city: 'Ciudad 1',
+      state: 'Estado 1',
+      zip: '12345',
+      country: 'Pais 1'
+    },
+    {
+      id: 2,
+      name: 'Proveedor 2',
+      email: 'proveedor2@gmail.com',
+      phone: '123456789',
+      address: 'Calle 123',
+      city: 'Ciudad 1',
+      state: 'Estado 1',
+      zip: '12345',
+      country: 'Pais 1'
+    },
+    {
+      id: 3,
+      name: 'Proveedor 3',
+      email: 'proveedor3@gmail.com',
+      phone: '123456789',
+      address: 'Calle 123',
+      city: 'Ciudad 1',
+      state: 'Estado 1',
+      zip: '12345',
+      country: 'Pais 1'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
+
+
+
+
+
 
 }
